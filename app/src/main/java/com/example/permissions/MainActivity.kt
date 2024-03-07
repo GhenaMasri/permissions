f251da577a7146c640484495e6f4aaaf2ac55743
GhenaMasri <ghenama77@gmail.com>
2024-03-07 14:51:15 +0200

    Change TargetSdk

--- app/src/main/java/com/example/permissions/MainActivity.kt
+++ app/src/main/java/com/example/permissions/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btnRequestPermissions.setOnClickListener {
-            requestPermissions()k
+            requestPermissions()
         }
     }
     private fun hasWriteExternalStoragePermission() =
